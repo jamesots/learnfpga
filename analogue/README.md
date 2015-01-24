9f4f07d67fac0ba0611ca18d322dcc5c0194a25c
James Ots <code@jamesots.com>
2015-01-24 21:26:30 +0000

    Timing is a bit better

--- analogue/README.md
+++ analogue/README.md
@@ -1,4 +1,7 @@
-This project reads from AIN2 and displays the value on the LEDs, in bargraph form.
+This project reads from AIN7 and displays the value on the LEDs, in bargraph form.
 
-You can change which port it reads from by changing the value of ad_port. Except you
-can't, as I haven't got the timing right yet, so the port chosen is a little random.
+You can change which port it reads from by changing the value of ad_port. 
+
+In theory I don't need to take CS high and low again, but I can't seem 
+to get the frame timing right without it. And even with it, sometimes the 
+port it reads from gets shifted. I need to do something about that.
--- analogue/vhdl/analogue.vhdl
+++ analogue/vhdl/analogue.vhdl
@@ -25,29 +25,27 @@ architecture Behavioral of analogue is
 
   signal value : STD_LOGIC_VECTOR (11 downto 0);
   signal step : integer range 0 to 20 := 0;
-  signal ad_port : STD_LOGIC_VECTOR (2 downto 0) := "010"; -- 2
+  signal ad_port : STD_LOGIC_VECTOR (2 downto 0) := "111"; 
   signal reset : STD_LOGIC := '0';
   signal clk : STD_LOGIC;
+  signal start : boolean := true;
 begin
   div : clock_divider port map (
     clk_in => clk50,
     clk_out => clk,
     reset => reset
   );
-  process(clk, reset)
+  process(clk)
   begin
       ad_sclk <= clk;
       if (rising_edge(clk)) then
-          if (step < 5) then
-            ad_cs <= '0';
-            step <= step + 1;
-            value <= "000000000000";
-          elsif (step < 16) then
+        if (start) then
+        else 
+          if (step >= 5 and step < 16) then
             -- dout is clocked out on the falling edge of the clock,
             -- so read it on the rising edge
 
             value(16 - step) <= ad_dout;
-            step <= step + 1;
           elsif (step = 16) then
             value(0) <= ad_dout;
             if (unsigned(value) < 256) then
@@ -69,17 +67,39 @@ begin
             else
               leds <= "11111111";
             end if;
-            step <= 1;
           end if;
+        end if;
       end if;
       if (falling_edge(clk)) then
-          if ((step >= 1) and (step < 4)) then
+        if (start) then
+          -- send cs high for one clock cycle to make sure
+          -- we know where our frames start
+          ad_cs <= '1';
+          -- 18 = not a normal step - used to send cs low and start the frame
+          step <= 18;
+          start <= false;
+        else 
+          step <= step + 1;
+          if (step = 18) then
+            step <= 1;
+            ad_cs <= '0';
+            ad_din <= '0';
+            value <= "000000000000";
+          elsif (step = 16) then
+            start <= true;
+            step <= 1;
+          elsif (step = 1) then
+            ad_cs <= '0';
+          end if;
+
+          if ((step >= 2) and (step < 5)) then
             -- din is sampled on rising edge of clock, so set it on
             -- the falling edge
-            ad_din <= ad_port(3 - step);
+            ad_din <= ad_port(4 - step);
           else
             ad_din <= '0';
           end if;
+        end if;
       end if;
   end process;
 end Behavioral;
